34b2779afa03be925e3d43390609a65cf8ede1e8
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-05-27 10:15:16 -0400

    Cogniti-V

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-curly-81100",
-    user: "andrei",
-    password: "",
-    database: "cogniti-v",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
